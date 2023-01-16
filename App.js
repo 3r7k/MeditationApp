0beb7407cbc81c74bd7bead24cce57451b6dc58c
Eryk <122016513+3r7k@users.noreply.github.com>
2023-01-16 23:13:15 +0100

    added select song function

--- App.js
+++ App.js
@@ -1,68 +1,81 @@
 const app = () => {
-    const song =  document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video')
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
 
-    //Sounds
-    const sounds = document.querySelectorAll('.sound-picker button');
-    //Time display
-    const timeDisplay = document.querySelector('.time-display');
-    const timeSelect = document.querySelectorAll('.time-select button');
-    //Outline length
-    const outlineLength = outline.getTotalLength();
-    
-    //Duration
-    let fDuration = 300;
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //Time display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+  //Outline length
+  const outlineLength = outline.getTotalLength();
 
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset = outlineLength;
+  //Duration
+  let fDuration = 300;
 
-    //play song
-    play.addEventListener("click", () =>{
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //select song
+  sounds.forEach(sound => {
+    sound.addEventListener('click', function(){
+        song.src = this.getAttribute('data-sound');
+        video.src = this.getAttribute('data-video');
         checkPlaying(song);
     });
+  });
 
-    //select sound
-    timeSelect.forEach(option => {
-        option.addEventListener('click', function(){
-            fDuration = this.getAttribute("data-time");
-        });
+  //play song
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //select sound
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `0${Math.floor(fDuration / 60)}:${Math.floor(
+        fDuration % 60
+      )}0`;
     });
+  });
 
-    //stop and play song
-    const checkPlaying = song =>{
-        if(song.paused){
-            song.play();
-            video.play();
-            play.src = './svg/pause.svg';
-        }else{
-            song.pause();
-            video.pause();
-            play.src = './svg/play.svg';
-        }
-    } 
+  //stop and play song
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
 
-    //animated the circle
-    song.ontimeupdate = () =>{
-        let currentTime = song.currentTime;
-        let elapse = fDuration - currentTime;
-        let seconds = Math.floor(elapse % 60);
-        let minutes = Math.floor(elapse / 60);
-    
+  //animated the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapse = fDuration - currentTime;
+    let seconds = Math.floor(elapse % 60);
+    let minutes = Math.floor(elapse / 60);
+
     //animate the circle
-    outline.style.strokeDashoffset = outlineLength - (currentTime/fDuration) * outlineLength;
-    
+    outline.style.strokeDashoffset =
+      outlineLength - (currentTime / fDuration) * outlineLength;
+
     //animate the text
     timeDisplay.textContent = `${minutes}:${seconds}`;
 
-    if(currentTime >= fDuration){
-        song.pause();
-        video.pause();
-        play.src = './svg/play.svg';
-        song.currentTime = 0;
+    if (currentTime >= fDuration) {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+      song.currentTime = 0;
     }
-    }
+  };
 };
 
 app();
